f6ca0fe57d05ba792d1493200ce7727feb399006
Gillian-B <44807459+Gillian-B@users.noreply.github.com>
2019-02-11 17:17:31 +0100

    lancement mode scientifique (#3)
    * ajout du mode normal de la calculatrice
    
    * ajout du mode normal de la calculatrice
    
    * Ajout lancement mode scientifique

--- calculate.js
+++ calculate.js
@@ -6,17 +6,17 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-scientific = function() {
+let scientific = function() {
 rl.question('> ', (answer) => {
     if(answer == "exit"){
         return rl.close()
     }
     let result = math.eval(answer)
     console.log(result)
-    calc();
+    scientific();
 });
 }
 
-exports.scientific() = function() {
+exports.scientific = function() {
     scientific();
 }
