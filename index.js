f6f8b5df9462194694c04189a87647779f06e644
Gillian Bruyas <44807459+Gillian-B@users.noreply.github.com>
2019-03-05 17:01:21 +0100

    Gillian (#7)
    * Changement message de lancement

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const getMode = require('./getMode');
 
+<<<<<<< HEAD
 console.log("Hello!")
+console.log('Bienvenue dans la calculatrice la plus puissante du monde')
+=======
+console.log('Bienvenue dans la calculatrice la plus puissante du monde !!!')
+>>>>>>> Changement message lancement'
 getMode.getMode()
 
 
