bb715099927fb80f603ac3ce91274f7dcb4da94e
Matthias <matthiaspinard@gmail.com>
2019-03-05 17:32:39 +0100

    fix eslint error

--- getMode.js
+++ getMode.js
@@ -1,32 +1,27 @@
-const readline = require('readline');
+const readline = require('readline')
 const normalMode = require('./normalMode')
 const calculate = require('./calculate')
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
-});
+})
 
-
-let getMode = function() {
+let getMode = function () {
   let mode
-    rl.question('Mode normal : 0 / Mode scientifique : 1 \n', (answer) => {
-      mode = answer
-      if(mode == 0){
-        console.log('Mode normal');
-        normalMode.normalMode()
-      }
-      else if(mode == 1){
-        console.log('Mode scientifique')
-        calculate.scientific()
-       }
-      else{
-        getMode()
-      }
-    });
+  rl.question('Mode normal : 0 / Mode scientifique : 1 \n', (answer) => {
+    mode = answer
+    if (mode == 0) {
+      console.log('Mode normal')
+      normalMode.normalMode()
+    } else if (mode == 1) {
+      console.log('Mode scientifique')
+      calculate.scientific()
+    } else {
+      getMode()
+    }
+  })
 }
-exports.getMode = function() {
+exports.getMode = function () {
   getMode()
 }
-
-
--- testNormalMode.test.js
+++ testNormalMode.test.js
@@ -1,17 +1,16 @@
 
-
 test('adds 1 + 2 to equal 3', () => {
-    expect(parseInt('1')+parseInt('2')).toBe(3);
-});
+  expect(parseInt('1') + parseInt('2')).toBe(3)
+})
 
 test('substract 5 - 2 to equal 3', () => {
-    expect(parseInt('5')-parseInt('2')).toBe(3);
-});
+  expect(parseInt('5') - parseInt('2')).toBe(3)
+})
 
 test('multiply 10 * 2 to equal 20', () => {
-    expect(parseInt('10')*parseInt('2')).toBe(20);
-});
-  
+  expect(parseInt('10') * parseInt('2')).toBe(20)
+})
+
 test('divid 100 / 2 to equal 50', () => {
-    expect(parseInt('100')/parseInt('2')).toBe(50);
-});
+  expect(parseInt('100') / parseInt('2')).toBe(50)
+})
